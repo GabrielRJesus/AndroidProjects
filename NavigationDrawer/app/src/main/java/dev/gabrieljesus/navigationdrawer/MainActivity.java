83aa9a8f316709f537e544b3af9a453eb8f80d4c
GabrielRJesus <gabrielrj623@hotmail.com>
2021-01-22 17:01:06 -0300

    Finalização drawer navigation

--- NavigationDrawer/app/src/main/java/dev/gabrieljesus/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/dev/gabrieljesus/navigationdrawer/MainActivity.java
@@ -1,5 +1,6 @@
 package dev.gabrieljesus.navigationdrawer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,11 +8,16 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
 
         toggle.syncState();
+
+        navigationView = findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -35,5 +44,33 @@ public class MainActivity extends AppCompatActivity {
         }else{
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+            case R.id.menu_inbox : {
+                Toast.makeText(this,"Inbox", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.menu_favoritos : {
+                Toast.makeText(this,"Favoritos", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.menu_sent_email : {
+                Toast.makeText(this,"Emails enviados", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.menu_trash : {
+                Toast.makeText(this,"Lixeira", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.menu_spam: {
+                Toast.makeText(this,"Spam", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
