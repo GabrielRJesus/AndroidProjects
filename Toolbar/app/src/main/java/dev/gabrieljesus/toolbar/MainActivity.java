2bd44360059b5f92505ff1dad7c1301b4ed6297a
GabrielRJesus <gabrielrj623@hotmail.com>
2021-01-22 10:45:02 -0300

    Click no menu

--- Toolbar/app/src/main/java/dev/gabrieljesus/toolbar/MainActivity.java
+++ Toolbar/app/src/main/java/dev/gabrieljesus/toolbar/MainActivity.java
@@ -1,10 +1,13 @@
 package dev.gabrieljesus.toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +26,28 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_toolbar, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.searchMenu: {
+                Toast.makeText(this, "Buscar",Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.cartMenu: {
+                Toast.makeText(this, "Carrinho",Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.configMenu: {
+                Toast.makeText(this, "Configurações",Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.aboutMenu: {
+                Toast.makeText(this, "Sobre",Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
