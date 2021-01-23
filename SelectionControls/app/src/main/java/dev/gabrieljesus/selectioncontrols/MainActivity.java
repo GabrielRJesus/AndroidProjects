6d6c49a999e67a278265902cfd55d6d46fde9b6f
GabrielRJesus <gabrielrj623@hotmail.com>
2021-01-22 21:50:37 -0300

    RadioGroup e RadioButton

--- SelectionControls/app/src/main/java/dev/gabrieljesus/selectioncontrols/MainActivity.java
+++ SelectionControls/app/src/main/java/dev/gabrieljesus/selectioncontrols/MainActivity.java
@@ -6,12 +6,14 @@ import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
 import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private SwitchCompat switchButton;
     private AppCompatCheckBox checkBox;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +41,27 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Gosta de sorvete", Toast.LENGTH_SHORT).show();
                 }else{
                     Toast.makeText(MainActivity.this, "Não gosta de sorvete", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch(i){
+                    case R.id.option1 : {
+                        Toast.makeText(MainActivity.this, "Opção 1", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.option2 : {
+                        Toast.makeText(MainActivity.this, "Opção 2", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.option3 : {
+                        Toast.makeText(MainActivity.this, "Opção 3", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
                 }
             }
         });
