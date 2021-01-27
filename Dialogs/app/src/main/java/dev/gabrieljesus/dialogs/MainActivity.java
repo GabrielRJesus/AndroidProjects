52795cb4242f82fc11166839ab368fd07920beb5
GabrielRJesus <gabrielrj623@hotmail.com>
2021-01-27 14:21:00 -0300

    Criação de DialogItens

--- Dialogs/app/src/main/java/dev/gabrieljesus/dialogs/MainActivity.java
+++ Dialogs/app/src/main/java/dev/gabrieljesus/dialogs/MainActivity.java
@@ -12,7 +12,11 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private AppCompatButton btnAlert;
+    private AppCompatButton btnDialogItens;
     private AlertDialog alertDialog;
+    private AlertDialog dialogItens;
+
+    private String[] items = {"Item1","Item2","Item3","Item4"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        //Dialog Alert
         AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialog);
         builder.setMessage("Deseja excluir?");
         builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
@@ -35,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         alertDialog = builder.create();
 
         btnAlert = findViewById(R.id.btn_alert);
@@ -43,6 +47,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 alertDialog.show();
+            }
+        });
+
+        //Dialog Items
+        AlertDialog.Builder builderDialogItens = new AlertDialog.Builder(this);
+        builderDialogItens.setTitle("Selecione a opção desejada:");
+//        builderDialogItens.setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                Toast.makeText(MainActivity.this, items[i], Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        builderDialogItens.setMultiChoiceItems(items, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                Toast.makeText(MainActivity.this, items[i] + " : " + b, Toast.LENGTH_SHORT).show();
+            }
+        });
+        builderDialogItens.setPositiveButton("Ok", null);
+        builderDialogItens.setNegativeButton("Cancelar", null);
+
+        dialogItens = builderDialogItens.create();
+
+        btnDialogItens = findViewById(R.id.btnDialogItens);
+        btnDialogItens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogItens.show();
             }
         });
     }
