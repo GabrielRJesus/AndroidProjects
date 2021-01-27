5d987ac97fa9b71d0b0ef436cc1b286f95e51fb2
GabrielRJesus <gabrielrj623@hotmail.com>
2021-01-27 14:53:05 -0300

    Criação Snackbar

---
+++ SnackbarToast/app/src/main/java/dev/gabrieljesus/snackbartoast/MainActivity.java
@@ -0,0 +1,31 @@
+package dev.gabrieljesus.snackbartoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final View relativeLayout = findViewById(R.id.activityMain);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar.make(relativeLayout, "ITEM EXCLUIDO",Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        });
+    }
+}
